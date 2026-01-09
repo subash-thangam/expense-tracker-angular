46dd3c73b0433db634d09d1e93e233935d9a2ff6
subash-thangam <subashthangadurai2000@gmail.com>
2026-01-09 12:33:05 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, isDevMode } from '@angular/core';
-import { SwUpdate } from '@angular/service-worker';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,24 +7,6 @@ import { SwUpdate } from '@angular/service-worker';
 })
 export class AppComponent {
   title = 'expense-tracker-angular';
-
-  constructor(private swUpdate: SwUpdate) {
-    if (!isDevMode()) {
-      // Check for updates every 6 hours
-      setInterval(() => {
-        this.swUpdate.checkForUpdate();
-      }, 6 * 60 * 60 * 1000);
-
-      // Listen for available updates
-      this.swUpdate.versionUpdates.subscribe(event => {
-        if (event.type === 'VERSION_READY') {
-          console.log('New app version available. Reloading...');
-          // Reload the page to update to the latest version
-          window.location.reload();
-        }
-      });
-    }
-  }
 }
 
 
